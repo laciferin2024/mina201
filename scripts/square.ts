21037334eaa664dcaff13f00a2b0cee75de77470
Nasfame <laciferin@gmail.com>
2024-10-13 15:18:23 +0400

    migrated: back to scripts

--- scripts/square.ts
+++ scripts/square.ts
@@ -10,7 +10,7 @@ import {
   AccountUpdate,
 } from 'o1js';
 
-import { Square } from './Square.js';
+import { Square } from '../src/Square.js';
 
 let deployerAccount: Mina.TestPublicKey,
   deployerKey: PrivateKey,
