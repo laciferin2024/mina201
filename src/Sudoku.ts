72cc943c32f4cad84389ec5afba0d59b20049fa4
Nasfame <laciferin@gmail.com>
2024-10-19 00:45:45 +0400

    sudoku class ready

--- src/Sudoku.ts
+++ src/Sudoku.ts
@@ -12,7 +12,15 @@ import {
 
 export class Sudoku extends Struct({
   value: Provable.Array(Provable.Array(Field, 9), 9),
-}) {}
+}) {
+  static from(value: number[][]) {
+    return new Sudoku({ value: value.map((row) => row.map(Field)) });
+  }
+
+  hash() {
+    return Poseidon.hash(this.value.flat());
+  }
+}
 
 export class SudokuZkApp extends SmartContract {
   @state(Field) sudokuHash = State<Field>();
@@ -26,4 +34,9 @@ export class SudokuZkApp extends SmartContract {
     this.sudokuHash.set(sudokuInstance.hash());
     this.isSolved.set(Bool(false));
   }
+
+  @method async submitSolution(
+    sudokuInstance: Sudoku,
+    solutionInstance: Sudoku
+  ) {}
 }
